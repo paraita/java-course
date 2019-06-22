b5ef7902ada13bf6fd84fc5afb69d37a629c56de
Paraita Wohler <paraita.wohler@gmail.com>
2019-06-21 20:11:06 -1000

    WIP partie sur les methodes

---
+++ 7_methode/Personne.java
@@ -0,0 +1,18 @@
+class Personne {
+
+    private String nom;
+    private String prenom;
+
+    Personne(String nom, String prenom) {
+	this.nom = nom;
+	this.prenom = prenom;
+    }
+
+    public 
+
+    public void presenteToi() {
+	System.out.println("Je m'appelle " +
+			   prenom + " " + nom);
+    }
+
+}
