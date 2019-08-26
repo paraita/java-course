578132ea631f52fdb538b83f2bfe4a54b4a7feea
Paraita Wohler <paraita.wohler@gmail.com>
2019-08-25 17:27:19 -1000

    Premier exemple d'interface: le chien joueur

---
+++ 15_interface/Chien.java
@@ -0,0 +1,11 @@
+class Chien extends Animal implements Joueur {
+    
+    public void donnesLaPatte() {
+	System.out.println("*donnes la patte*");
+    }
+
+    public void vaChercher() {
+	System.out.println("va chercher le baton");
+    }
+    
+}
---
+++ 15_interface/Exemple17.java
@@ -0,0 +1,7 @@
+class Exemple17 {
+    public static void main(String[] args) {
+	Chien cali = new Chien();
+	System.out.println("Cali: " + cali);
+	cali.donnesLaPatte();
+    }
+}
